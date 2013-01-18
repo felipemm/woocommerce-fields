34f572519676b3ada88a3da8a1a9562707fe066b
felipemm <felipemm@abe3dd39-df4f-4918-8f21-64e65b2f162d>
2013-01-18 04:03:32 +0000

    - v2.0 - added automatic update from private server - added custom fields in the user profile page - added javascript injection for field validation in the checkout and edit address pages
    git-svn-id: http://svn.felipematos.com/projects/woocommerce-fields@161 abe3dd39-df4f-4918-8f21-64e65b2f162d

--- trunk/woocommerce-fields.php
+++ trunk/woocommerce-fields.php
@@ -1,14 +1,233 @@
 <?php
 /*
 Plugin Name: WooCommerce Fields
-Plugin URI: http://felipematos.com/loja
-Description: Customiza a página de checkout e de endereço.
-Version: 1.0
+Plugin URI: http://wooplugins.com.br/loja/woocommerce-fields/
+Description: Customiza a página de checkout e de endereço e coloca validações javascript nos campos.
+Version: 2.0
 Author: Felipe Matos <chucky_ath@yahoo.com.br>
 Author URI: http://felipematos.com
 Requires at least: 3.4
-Tested up to: 3.4.2
+Tested up to: 3.5
 */
+
+
+//-------------------------------------------------------------------------------------------
+// ##### PLUGIN AUTO UPDATE CODE #####
+//-------------------------------------------------------------------------------------------
+
+//Making sure wordpress does not check this plugin into their repository
+add_filter( 'http_request_args', 'wc_fields_prevent_update_check', 10, 2 );
+function wc_fields_prevent_update_check( $r, $url ) {
+    if ( 0 === strpos( $url, 'http://api.wordpress.org/plugins/update-check/' ) ) {
+        $my_plugin = plugin_basename( __FILE__ );
+        $plugins = unserialize( $r['body']['plugins'] );
+        unset( $plugins->plugins[$my_plugin] );
+        unset( $plugins->active[array_search( $my_plugin, $plugins->active )] );
+        $r['body']['plugins'] = serialize( $plugins );
+    }
+    return $r;
+}
+
+
+// TEMP: Enable update check on every request. Normally you don't need this! This is for testing only!
+// NOTE: The 
+//	if (empty($checked_data->checked))
+//		return $checked_data; 
+// lines will need to be commented in the check_for_plugin_update function as well.
+get_site_transient( 'update_plugins' ); // unset the plugin
+set_site_transient( 'update_plugins', '' ); // reset plugin database information
+// TEMP: Show which variables are being requested when query plugin API
+//add_filter('plugins_api_result', 'wc_fields_result', 10, 3);
+//function wc_fields_result($res, $action, $args) {
+//	print_r($res);
+//	return $res;
+//}
+// NOTE: All variables and functions will need to be prefixed properly to allow multiple plugins to be updated
+
+$api_url = 'http://update.wooplugins.com.br';
+$plugin_slug = basename(dirname(__FILE__));
+
+// Take over the update check
+add_filter('pre_set_site_transient_update_plugins', 'wc_fields_check_for_plugin_update');
+
+function wc_fields_check_for_plugin_update($checked_data) {
+	global $api_url, $plugin_slug;
+	
+	//Comment out these two lines during testing.
+	if (empty($checked_data->checked))
+		return $checked_data;
+	
+	$args = array(
+		'slug' => $plugin_slug,
+		'version' => $checked_data->checked[$plugin_slug .'/'. $plugin_slug .'.php'],
+	);
+	$request_string = array(
+			'body' => array(
+				'action' => 'basic_check', 
+				'request' => serialize($args),
+				'api-key' => md5(get_bloginfo('url'))
+			),
+			'user-agent' => 'WordPress/' . $wp_version . '; ' . get_bloginfo('url')
+		);
+	
+	// Start checking for an update
+	$raw_response = wp_remote_post($api_url, $request_string);
+	
+	if (!is_wp_error($raw_response) && ($raw_response['response']['code'] == 200))
+		$response = unserialize($raw_response['body']);
+	
+	if (is_object($response) && !empty($response)) // Feed the update data into WP updater
+		$checked_data->response[$plugin_slug .'/'. $plugin_slug .'.php'] = $response;
+	
+	return $checked_data;
+}
+
+
+// Take over the Plugin info screen
+add_filter('plugins_api', 'wc_fields_plugin_api_call', 10, 3);
+
+function wc_fields_plugin_api_call($def, $action, $args) {
+	global $plugin_slug, $api_url;
+	
+	if ($args->slug != $plugin_slug)
+		return false;
+	
+	// Get the current version
+	$plugin_info = get_site_transient('update_plugins');
+	$current_version = $plugin_info->checked[$plugin_slug .'/'. $plugin_slug .'.php'];
+	$args->version = $current_version;
+	
+	$request_string = array(
+			'body' => array(
+				'action' => $action, 
+				'request' => serialize($args),
+				'api-key' => md5(get_bloginfo('url'))
+			),
+			'user-agent' => 'WordPress/' . $wp_version . '; ' . get_bloginfo('url')
+		);
+	
+	$request = wp_remote_post($api_url, $request_string);
+	
+	if (is_wp_error($request)) {
+		$res = new WP_Error('plugins_api_failed', __('An Unexpected HTTP Error occurred during the API request.</p> <p><a href="?" onclick="document.location.reload(); return false;">Try again</a>'), $request->get_error_message());
+	} else {
+		$res = unserialize($request['body']);
+		
+		if ($res === false)
+			$res = new WP_Error('plugins_api_failed', __('An unknown error occurred'), $request['body']);
+	}
+	
+	return $res;
+}
+//-------------------------------------------------------------------------------------------
+// ##### PLUGIN AUTO UPDATE CODE #####
+//-------------------------------------------------------------------------------------------
+
+
+
+
+add_action('wp_head', 'wc_fields_add_js');
+
+function wc_fields_add_js(){
+	//$plugin_url = get_settings('home').'/wp-content/plugins/'.dirname(plugin_basename(__FILE__));
+	//wp_enqueue_script('loadjs', $plugin_url . '/example.js');
+	
+	if(is_page('finalizar-compra') || is_page('checkout')){
+		$plugin_url = plugin_dir_url(__FILE__) . 'woocommerce_checkout.js';
+	}
+	if(is_page('editar-endereco') || is_page('edit-address')){
+		$plugin_url = plugin_dir_url(__FILE__) . 'woocommerce_edit_address.js';
+	}
+	
+	wp_enqueue_script('loadjs', $plugin_url);
+	//echo $plugin_url;
+}
+
+
+//-----------------------------------------------------------------------------------------
+// ### UPDATE AND SHOW CUSTOM USER INFO ###
+//-----------------------------------------------------------------------------------------
+add_action( 'show_user_profile', 'my_show_extra_profile_fields' );
+add_action( 'edit_user_profile', 'my_show_extra_profile_fields' );
+
+function my_show_extra_profile_fields( $user ) { 
+	?>
+	<h3>WooCommerce Extra Fields</h3>
+	<table class="form-table">
+		<tr>
+			<th><label for="billing_cpf">CPF</label></th>
+			<td>
+				<input type="text" name="billing_cpf" id="billing_cpf" value="<?php echo esc_attr( get_the_author_meta( 'billing_cpf', $user->ID ) ); ?>" class="regular-text" />
+				<br/>
+				<span class="description">Digite o seu CPF.</span>
+			</td>
+		</tr>
+		<tr>
+			<th><label for="billing_rg">RG</label></th>
+			<td>
+				<input type="text" name="billing_rg" id="billing_rg" value="<?php echo esc_attr( get_the_author_meta( 'billing_rg', $user->ID ) ); ?>" class="regular-text" />
+				<br/>
+				<span class="description">Digite o seu RG.</span>
+			</td>
+		</tr>
+		<tr>
+			<th><label for="billing_nascimento">Data de Nascimento</label></th>
+			<td>
+				<input type="text" name="billing_nascimento" id="billing_nascimento" value="<?php echo esc_attr( get_the_author_meta( 'billing_nascimento', $user->ID ) ); ?>" class="regular-text" />
+				<br/>
+				<span class="description">Digite sua data de nascimento</span>
+			</td>
+		</tr>
+		<tr>
+			<th><label for="billing_number">Número da Casa</label></th>
+			<td>
+				<input type="text" name="billing_number" id="billing_number" value="<?php echo esc_attr( get_the_author_meta( 'billing_number', $user->ID ) ); ?>" class="regular-text" />
+				<br/>
+				<span class="description">Digite o número da sua casa</span>
+			</td>
+		</tr>
+		<tr>
+			<th><label for="billing_district">Bairro</label></th>
+			<td>
+				<input type="text" name="billing_district" id="billing_district" value="<?php echo esc_attr( get_the_author_meta( 'billing_district', $user->ID ) ); ?>" class="regular-text" />
+				<br/>
+				<span class="description">Digite o seu bairro</span>
+			</td>
+		</tr>
+		<tr>
+			<th><label for="billing_cellphone">Celular</label></th>
+			<td>
+				<input type="text" name="billing_cellphone" id="billing_cellphone" value="<?php echo esc_attr( get_the_author_meta( 'billing_cellphone', $user->ID ) ); ?>" class="regular-text" />
+				<br/>
+				<span class="description">Digite o seu celular</span>
+			</td>
+		</tr>
+		<tr>
+	</table>
+	<?php 
+}
+
+add_action( 'personal_options_update', 'my_save_extra_profile_fields' );
+add_action( 'edit_user_profile_update', 'my_save_extra_profile_fields' );
+
+function my_save_extra_profile_fields( $user_id ) {
+	if ( !current_user_can( 'edit_user', $user_id ) ) return false;
+	
+	/* Copy and paste this line for additional fields. Make sure to change 'twitter' to the field ID. */
+	update_usermeta( $user_id, 'billing_rg', $_POST['billing_rg'] );
+	update_usermeta( $user_id, 'billing_cpf', $_POST['billing_cpf'] );
+	update_usermeta( $user_id, 'billing_nascimento', $_POST['billing_nascimento'] );
+	update_usermeta( $user_id, 'billing_number', $_POST['billing_number'] );
+	update_usermeta( $user_id, 'billing_district', $_POST['billing_district'] );
+	update_usermeta( $user_id, 'billing_cellphone', $_POST['billing_cellphone'] );
+}
+//-----------------------------------------------------------------------------------------
+// ### UPDATE AND SHOW CUSTOM USER INFO ###
+//-----------------------------------------------------------------------------------------
+
+
+
+
 
 add_filter( 'woocommerce_billing_fields' , 'custom_override_billing_fields' );
 
@@ -197,6 +416,7 @@ function custom_override_billing_fields($fields){
 add_filter( 'woocommerce_checkout_fields' , 'custom_override_checkout_fields' );
 
 function custom_override_checkout_fields( $fields ) {
+
 
 	$new_fields = array(
 		'billing_first_name' => array( 
---
+++ trunk/woocommerce_edit_address.js
@@ -0,0 +1,87 @@
+﻿function mascara(o,f){  
+	v_obj=o  
+	v_fun=f  
+	setTimeout("execmascara()",1)  
+}  
+function execmascara(){  
+	v_obj.value=v_fun(v_obj.value)  
+}  
+function mtel(v){  
+	v=v.replace(/\D/g,"");             //Remove tudo o que não é dígito  
+	v=v.replace(/^(\d{2})(\d)/g,"($1)$2"); //Coloca parênteses em volta dos dois primeiros dígitos  
+	v=v.replace(/(\d)(\d{4})$/,"$1-$2");    //Coloca hífen entre o quarto e o quinto dígitos  
+	return v;  
+}  
+//valida numero inteiro com mascara
+function mascaraInteiro(el, event){
+	if (event.keyCode < 48 || event.keyCode > 57){
+		event.returnValue = false;
+		return false;
+	}
+	return true;
+}
+//formata de forma generica os campos
+function formataCampo(campo, Mascara, evento) {
+	var boleanoMascara;
+	var Digitato = evento.keyCode;
+	exp = /\-|\.|\/|\(|\)| /g
+	campoSoNumeros = campo.value.toString().replace( exp, '' );
+	var posicaoCampo = 0;
+	var NovoValorCampo='';
+	var TamanhoMascara = campoSoNumeros.length;;
+	if (Digitato != 8) { // backspace
+		for(i=0; i<= TamanhoMascara; i++) {
+			boleanoMascara  = ((Mascara.charAt(i) == '-') || (Mascara.charAt(i) == '.') || (Mascara.charAt(i) == '/'))
+			boleanoMascara  = boleanoMascara || ((Mascara.charAt(i) == '(') || (Mascara.charAt(i) == ')') || (Mascara.charAt(i) == ' '))
+			if (boleanoMascara) {
+				NovoValorCampo += Mascara.charAt(i);
+				TamanhoMascara++;
+			}else {
+				NovoValorCampo += campoSoNumeros.charAt(posicaoCampo);
+				posicaoCampo++;
+			}
+		}
+		campo.value = NovoValorCampo;
+		return true;
+	} else {
+		return true;
+	}
+}
+//função para validar a máscara do campo de cep
+function MascaraCEP(cep,event){
+	if(mascaraInteiro(cep,event)==false){
+		event.returnValue = false;
+	}
+	return formataCampo(cep, '00000-000', event);
+}
+function MascaraCPF(cpf,event){
+	if(mascaraInteiro(cpf,event)==false){
+		event.returnValue = false;
+	}
+	return formataCampo(cpf, '000.000.000-00', event);
+}
+function MascaraData(data,event){
+	if(mascaraInteiro(data,event)==false){
+		event.returnValue = false;
+	}
+	return formataCampo(data, '00/00/0000', event);
+}
+//==============================================================================
+//variáveis dos campos a serem alterados
+var cep      = document.getElementById('billing_postcode');
+var celular = document.getElementById('billing_cellphone');	     	
+var telefone = document.getElementById('billing_phone');	     	
+var cpf = document.getElementById('billing_cpf');	     	
+var data_nasc= document.getElementById('billing_nascimento');	     	
+//==============================================================================
+//coloca as validações nos campos de cep e telefone
+cep.maxLength=9;
+cep.onkeypress= function(event){MascaraCEP(this, event);};
+telefone.maxLength=14;
+telefone.onkeydown= function(event){mascara(this,mtel);};
+celular.maxLength=14;
+celular.onkeydown= function(event){mascara(this,mtel);};
+cpf.maxLength=14;
+cpf.onkeypress= function(event){MascaraCPF(this,event);};
+data_nasc.maxLength=10;
+data_nasc.onkeypress= function(event){MascaraData(this,event);};
